d0bb36266880c98dba51bb85d327e69459daa3ba
fishyWET <fishyWET@hotmail.com>
2014-02-17 18:51:30 +0800

    Changed spawning of ores
    And also removed light as it is seemingly easy to find now

--- lottseaores/init.lua
+++ lottseaores/init.lua
@@ -4,7 +4,6 @@ minetest.register_node("lottseaores:mineral_pearl", {
 	description = "Pearl Ore",
 	tiles = {"default_sand.png^lottseaores_mineral_pearl.png"},
 	paramtype = "light",
-	light_source = 5,
 	is_ground_content = true,
 	groups = {snappy=2,cracky=3,oddly_breakable_by_hand=3},
 
@@ -23,9 +22,8 @@ minetest.register_node("lottseaores:mineral_pearl", {
 
 minetest.register_node("lottseaores:mineral_salt", {
 	description = "Salt Mineral",
-	tiles = {"default_dirt.png^lottseaores_mineral_salt.png"},
+	tiles = {"default_sand.png^lottseaores_mineral_salt.png"},
 	paramtype = "light",
-	light_source = 6,
 	is_ground_content = true,
 	groups = {snappy=2,cracky=3,oddly_breakable_by_hand=3},
 
@@ -122,43 +120,48 @@ stairs.register_stair_and_slab("salt", "lottseaores:salt_block",
 
 -- Ore generation
 
-local function generate_ore(name, wherein, minp, maxp, seed, chunks_per_volume, chunk_size, ore_per_chunk, height_min, height_max)
-	if maxp.y < height_min or minp.y > height_max then
-		return
-	end
-	local y_min = math.max(minp.y, height_min)
-	local y_max = math.min(maxp.y, height_max)
-	local volume = (maxp.x-minp.x+1)*(y_max-y_min+1)*(maxp.z-minp.z+1)
-	local pr = PseudoRandom(seed)
-	local num_chunks = math.floor(chunks_per_volume * volume)
-	local inverse_chance = math.floor(chunk_size*chunk_size*chunk_size / ore_per_chunk)
-	--print("generate_ore num_chunks: "..dump(num_chunks))
-	for i=1,num_chunks do
-		local y0 = pr:next(y_min, y_max-chunk_size+1)
-		if y0 >= height_min and y0 <= height_max then
-			local x0 = pr:next(minp.x, maxp.x-chunk_size+1)
-			local z0 = pr:next(minp.z, maxp.z-chunk_size+1)
-			local p0 = {x=x0, y=y0, z=z0}
-			for x1=0,chunk_size-1 do
-			for y1=0,chunk_size-1 do
-			for z1=0,chunk_size-1 do
-				if pr:next(1,inverse_chance) == 1 then
-					local x2 = x0+x1
-					local y2 = y0+y1
-					local z2 = z0+z1
-					local p2 = {x=x2, y=y2, z=z2}
-					if minetest.env:get_node(p2).name == wherein then
-						minetest.env:set_node(p2, {name=name})
-					end
-				end
-			end
-			end
-			end
-		end
-	end
-end
+minetest.register_ore({
+	ore_type       = "scatter",
+	ore            = "lottseaores:mineral_salt",
+	wherein        = "default:stone",
+	clust_scarcity = 12*12*12,
+	clust_num_ores = 2,
+	clust_size     = 2,
+	height_min     = -100,
+	height_max     = 10,
+})
 
-minetest.register_on_generated(function(minp, maxp, seed)
-	generate_ore("lottseaores:mineral_salt", "default:dirt", minp, maxp, seed+20, 1/7/7/7, 1, 1, -31000, -6)
-	generate_ore("lottseaores:mineral_pearl", "default:sand", minp, maxp, seed+21, 1/7/7/7, 1, 1, -31000, -6)
-end)
+minetest.register_ore({
+	ore_type       = "scatter",
+	ore            = "lottseaores:mineral_salt",
+	wherein        = "default:stone",
+	clust_scarcity = 9*9*9,
+	clust_num_ores = 2,
+	clust_size     = 5,
+	height_min     = -100,
+	height_max     = 20,
+	flags          = "absheight",
+})
+
+minetest.register_ore({
+	ore_type       = "scatter",
+	ore            = "lottseaores:mineral_pearl",
+	wherein        = "default:sand",
+	clust_scarcity = 12*12*12,
+	clust_num_ores = 2,
+	clust_size     = 2,
+	height_min     = -100,
+	height_max     = 10,
+})
+
+minetest.register_ore({
+	ore_type       = "scatter",
+	ore            = "lottseaores:mineral_pearl",
+	wherein        = "default:sand",
+	clust_scarcity = 9*9*9,
+	clust_num_ores = 5,
+	clust_size     = 5,
+	height_min     = -100,
+	height_max     = 20,
+	flags          = "absheight",
+})
