dd23f18bf8a0c2c8fda8bae9ea18131c7c01c56e
Amaz1 <charliejordan34@gmail.com>
2014-02-21 11:27:26 +0000

    Removed non working mod

--- lottthrowing/init.lua
+++
@@ -1,7 +0,0 @@
-dofile(minetest.get_modpath("lottthrowing").."/arrow.lua")
-dofile(minetest.get_modpath("lottthrowing").."/axe.lua")
-dofile(minetest.get_modpath("lottthrowing").."/bow.lua")
-
-if minetest.setting_get("log_mods") then
-	minetest.log("action", "lottthrowing loaded")
-end
