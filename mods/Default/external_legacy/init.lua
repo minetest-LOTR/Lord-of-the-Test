8e0911d9b1ce67e29ad5c253844252923cd0e5bd
fishyWET <fishyWET@hotmail.com>
2014-11-17 18:51:27 +0800

    Updatezilla Phrase 1
    Massive Overhaul Commit 2

---
+++ mods/Default/external_legacy/init.lua
@@ -0,0 +1,44 @@
+-- Minetest 0.4 mod: external_legacy
+-- See README.txt for licensing and other information.
+
+-- Aliases to support moreores' ores
+minetest.register_alias("moreores:mineral_gold", "default:stone_with_gold")
+minetest.register_alias("moreores:gold_block", "default:goldblock")
+minetest.register_alias("moreores:gold_lump", "default:gold_lump")
+minetest.register_alias("moreores:gold_ingot", "default:gold_ingot")
+minetest.register_alias("moreores:mineral_copper", "default:stone_with_copper")
+minetest.register_alias("moreores:copper_lump", "default:copper_lump")
+minetest.register_alias("moreores:copper_ingot", "default:copper_ingot")
+minetest.register_alias("moreores:copper_block", "default:copperblock")
+minetest.register_alias("moreores:bronze_ingot", "default:bronze_ingot")
+minetest.register_alias("moreores:bronze_block", "default:bronzeblock")
+
+-- Aliases for the diamonds mod by InfinityProject
+minetest.register_alias("diamonds:diamond_in_ground", "default:stone_with_diamond")
+minetest.register_alias("diamonds:block", "default:diamondblock")
+minetest.register_alias("diamonds:sword", "default:sword_diamond")
+minetest.register_alias("diamonds:pick", "default:pick_diamond")
+minetest.register_alias("diamonds:shovel", "default:shovel_diamond")
+minetest.register_alias("diamonds:axe", "default:axe_diamond")
+minetest.register_alias("diamonds:diamond", "default:diamond")
+minetest.register_alias("diamonds:ingot", "default:diamond")
+
+minetest.register_alias("lottspecial:helmet_birthday", "lottarmor:helmet_bronze")
+minetest.register_alias("lottspecial:chestplate_birthday", "lottarmor:chestplate_bronze")
+minetest.register_alias("lottspecial:leggings_birthday", "lottarmor:leggings_bronze")
+minetest.register_alias("lottspecial:boots_birthday", "lottarmor:boots_bronze")
+minetest.register_alias("lottspecial:shield_birthday", "lottarmor:shield_bronze")
+minetest.register_alias("lottspecial:birthday_paxel", "lottores:mithrilpick")
+minetest.register_alias("lottspecial:chest", "default:chest")
+minetest.register_alias("lottspecial:cake_knife", "default:sword_steel")
+minetest.register_alias("lottspecial:cake_slice", "default:apple")
+minetest.register_alias("lottspecial:cake", "default:bread")
+
+minetest.register_alias("hatches:hatch_alderwood_open", "lottblocks:hatch_alder_open")
+minetest.register_alias("hatches:hatch_alderwood", "lottblocks:hatch_alder")
+minetest.register_alias("hatches:hatch_birchwood_open", "lottblocks:hatch_birch_open")
+minetest.register_alias("hatches:hatch_birchwood", "lottblocks:hatch_birch")
+minetest.register_alias("hatches:hatch_lebethronwood_open", "lottblocks:hatch_lebethron_open")
+minetest.register_alias("hatches:hatch_lebethronwood", "lottblocks:hatch_lebethron")
+minetest.register_alias("hatches:hatch_mallornwood_open", "lottblocks:hatch_mallorn_open")
+minetest.register_alias("hatches:hatch_mallornwood", "lottblocks:hatch_mallorn")
---
+++ mods/Default/screwdriver/init.lua
@@ -0,0 +1,84 @@
+local function nextrange(x, max)
+	x = x + 1
+	if x > max then
+		x = 0
+	end
+	return x
+end
+
+-- Handles rotation
+local function screwdriver_handler(itemstack, user, pointed_thing, mode)
+	if pointed_thing.type ~= "node" then
+		return
+	end
+
+	local pos = pointed_thing.under
+	local keys = user:get_player_control()
+	local player_name = user:get_player_name()
+
+	if minetest.is_protected(pos, user:get_player_name()) then
+		minetest.record_protection_violation(pos, user:get_player_name())
+		return
+	end
+
+	local node = minetest.get_node(pos)
+	local ndef = minetest.registered_nodes[node.name]
+	if not ndef or not ndef.paramtype2 == "facedir" or
+			(ndef.drawtype == "nodebox" and
+			not ndef.node_box.type == "fixed") or
+			node.param2 == nil then
+		return
+	end
+
+	-- Set param2
+	local n = node.param2
+	local axisdir = math.floor(n / 4)
+	local rotation = n - axisdir * 4
+	if mode == 1 then
+		n = axisdir * 4 + nextrange(rotation, 3)
+	elseif mode == 3 then
+		n = nextrange(axisdir, 5) * 4
+	end
+	
+	node.param2 = n
+	minetest.swap_node(pos, node)
+
+	local item_wear = tonumber(itemstack:get_wear())
+	item_wear = item_wear + 300 -- was 327
+	if item_wear > 65535 then
+		itemstack:clear()
+		return itemstack
+	end
+	itemstack:set_wear(item_wear)
+	return itemstack
+end
+
+-- Screwdriver
+minetest.register_tool("screwdriver:screwdriver", {
+	description = "Screwdriver (left-click rotates face, right-click rotates axis)",
+	inventory_image = "screwdriver.png",
+	on_use = function(itemstack, user, pointed_thing)
+		screwdriver_handler(itemstack, user, pointed_thing, 1)
+		return itemstack
+	end,
+	on_place = function(itemstack, user, pointed_thing)
+		screwdriver_handler(itemstack, user, pointed_thing, 3)
+		return itemstack
+	end,
+})
+
+
+minetest.register_craft({
+    output = 'screwdriver:screwdriver',
+	recipe = {
+		{'', '', 'default:steel_ingot'},
+		{'', 'default:steel_ingot', ''},
+		{'group:stick', '', ''},
+	}
+})
+
+-- Compatibility with original mod
+minetest.register_alias("screwdriver:screwdriver1", "screwdriver:screwdriver")
+minetest.register_alias("screwdriver:screwdriver2", "screwdriver:screwdriver")
+minetest.register_alias("screwdriver:screwdriver3", "screwdriver:screwdriver")
+minetest.register_alias("screwdriver:screwdriver4", "screwdriver:screwdriver")
