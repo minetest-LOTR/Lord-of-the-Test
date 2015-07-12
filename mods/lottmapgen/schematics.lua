5de0387d118ee6212ac1cd3d114602ff360d9ad4
Amaz <Amaz1@users.noreply.github.com>
2015-07-12 21:56:20 +0100

    Add a setting for protecting houses. protect_structures = true/false (true by default)

--- mods/lottmapgen/schematics.lua
+++ mods/lottmapgen/schematics.lua
@@ -1,5 +1,8 @@
 lottmapgen = {}
 
+local areas = areas
+local protect_houses = minetest.setting_getbool("protect_structures") or true
+
 local lottmapgen_list = {
     { "Angmar Fort", "angmarfort"},
     { "Gondor Fort", "gondorfort"},
@@ -13,8 +16,7 @@ local lottmapgen_list = {
 for i in ipairs(lottmapgen_list) do
     local builddesc = lottmapgen_list[i][1]
     local build = lottmapgen_list[i][2]
-
-minetest.register_node("lottmapgen:"..build, {
+    minetest.register_node("lottmapgen:"..build, {
         description = builddesc,
         drawtype = "glasslike",
         walkable = false,
@@ -52,7 +54,7 @@ minetest.register_abm({
                p.x = p.x - 5
                p.z = p.z - 2
                local count = worldedit.deserialize(pos, value)
-               if minetest.get_modpath("areas") ~= nil then
+               if areas ~= nil and protect_houses == true then
                     local pos1 = {x = pos.x - 3, y = pos.y - 15, z = pos.z - 3}
                     local pos2 = {x = pos.x + 10, y = pos.y + 35, z = pos.z + 10}
                     areas:add("Elven Guard", "Elven House", pos1, pos2, nil)
@@ -75,7 +77,7 @@ minetest.register_abm({
                p.x = p.x - 5
                p.z = p.z - 2
                local count = worldedit.deserialize(pos, value)
-               if minetest.get_modpath("areas") ~= nil then
+               if areas ~= nil and protect_houses == true then
                    local pos1 = {x = pos.x - 4, y = pos.y - 15, z = pos.z - 4}
                    local pos2 = {x = pos.x + 22, y = pos.y + 25, z = pos.z + 22}
                    areas:add("Orc Guard", "Angmar Fort", pos1, pos2, nil)
@@ -98,7 +100,7 @@ minetest.register_abm({
                p.x = p.x - 5
                p.z = p.z - 2
                local count = worldedit.deserialize(pos, value)
-               if minetest.get_modpath("areas") ~= nil then
+               if areas ~= nil and protect_houses == true then
                    local pos1 = {x = pos.x + 2, y = pos.y - 15, z = pos.z - 5}
                    local pos2 = {x = pos.x + 23, y = pos.y + 35, z = pos.z + 24}
                    areas:add("Gondor Guard", "Gondor Castle", pos1, pos2, nil)
@@ -121,7 +123,7 @@ minetest.register_abm({
                p.x = p.x - 5
                p.z = p.z - 2
                local count = worldedit.deserialize(pos, value)
-               if minetest.get_modpath("areas") ~= nil then
+               if areas ~= nil and protect_houses == true then
                    local pos1 = {x = pos.x, y = pos.y - 15, z = pos.z}
                    local pos2 = {x = pos.x + 30, y = pos.y + 10, z = pos.z + 20}
                    areas:add("Hobbit Family", "Hobbit Hole", pos1, pos2, nil)
@@ -144,7 +146,7 @@ minetest.register_abm({
                p.x = p.x - 5
                p.z = p.z - 2
                local count = worldedit.deserialize(pos, value)
-               if minetest.get_modpath("areas") ~= nil then
+               if areas ~= nil and protect_houses == true then
                    local pos1 = {x = pos.x - 4, y = pos.y - 15, z = pos.z - 4}
                    local pos2 = {x = pos.x + 26, y = pos.y + 45, z = pos.z + 26}
                    areas:add("Orc Guard", "Orc Fort", pos1, pos2, nil)
@@ -167,7 +169,7 @@ minetest.register_abm({
                p.x = p.x - 5
                p.z = p.z - 2
                local count = worldedit.deserialize(pos, value)
-               if minetest.get_modpath("areas") ~= nil then
+               if areas ~= nil and protect_houses == true then
                    local pos1 = {x = pos.x - 4, y = pos.y - 15, z = pos.z - 4}
                    local pos2 = {x = pos.x + 15, y = pos.y + 30, z = pos.z + 15}
                    areas:add("Elven Guard", "Elven House", pos1, pos2, nil)
@@ -190,7 +192,7 @@ minetest.register_abm({
                p.x = p.x - 5
                p.z = p.z - 2
                local count = worldedit.deserialize(pos, value)
-               if minetest.get_modpath("areas") ~= nil then
+               if areas ~= nil and protect_houses == true then
                    local pos1 = {x = pos.x - 4, y = pos.y - 15, z = pos.z - 4}
                    local pos2 = {x = pos.x + 29, y = pos.y + 25, z = pos.z + 29}
                    areas:add("Rohan Guard", "Rohan Fort", pos1, pos2, nil)
