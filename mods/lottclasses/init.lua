76dcd6823fe2e883bfcfc87bb43dd8479bb7ec8b
Amaz <Amaz1@users.noreply.github.com>
2015-08-26 22:23:26 +0100

    Add randomish starting stuff for players.

--- mods/lottclasses/init.lua
+++ mods/lottclasses/init.lua
@@ -46,57 +46,227 @@ local race_chooser = "size[8,6]"..
 	"button_exit[1,3.5;2,0.5;hobbit;Hobbit]"..
 	"dropdown[5.5,3.4;2;gender;Male,Female;1]"
 
+chance = 0
+
+local function regen_chance()
+	chance = math.random(1, 6)
+end
+
 local function give_stuff_dwarf(player)
-	player:get_inventory():add_item('main', 'lottores:mithrilpick')
-	player:get_inventory():add_item('main', 'lottweapons:mithril_battleaxe')
-	player:get_inventory():add_item('main', 'lottores:mithrilaxe')
-	player:get_inventory():add_item('main', 'default:shovel_steel')
-	player:get_inventory():add_item('main', 'lottarmor:chestplate_mithril')
+	regen_chance()
+	if chance == 1 then
+		player:get_inventory():add_item('main', 'lottores:mithrilpick')
+	elseif chance == 2 or chance == 3 then
+		player:get_inventory():add_item('main', 'lottores:tinpick')
+	elseif chance == 4 then
+		player:get_inventory():add_item('main', 'lottores:galvornpick')
+	elseif chance >= 5 then
+		player:get_inventory():add_item('main', 'default:pick_steel')
+	end
+	regen_chance()
+	if chance == 1 then
+		player:get_inventory():add_item('main', 'lottores:copperaxe')
+		player:get_inventory():add_item('main', 'lottweapons:gold_spear')
+	elseif chance == 2 then
+		player:get_inventory():add_item('main', 'lottores:mithrilaxe')
+		player:get_inventory():add_item('main', 'lottweapons:bronze_warhammer')
+	elseif chance == 3 or chance == 4 then
+		player:get_inventory():add_item('main', 'default:axe_steel')
+		player:get_inventory():add_item('main', 'lottweapons:silver_spear')
+	elseif chance == 5 then
+		player:get_inventory():add_item('main', 'lottweapons:mithril_battleaxe')
+	elseif chance == 6 then
+		player:get_inventory():add_item('main', 'default:axe_steel')
+		player:get_inventory():add_item('main', 'default:sword_steel')
+	end
+	regen_chance()
+	if chance <= 3 then
+		player:get_inventory():add_item('main', 'default:shovel_stone')
+	elseif chance == 4 then
+		player:get_inventory():add_item('main', 'default:shovel_steel')
+	elseif chance >= 5 then
+		player:get_inventory():add_item('main', 'lottores:coppershovel')
+	end
+	regen_chance()
+	if chance == 1 then
+		player:get_inventory():add_item('main', 'lottarmor:chestplate_bronze')
+	elseif chance == 2 then
+		player:get_inventory():add_item('main', 'lottarmor:chestplate_steel')
+	elseif chance == 3 then
+		player:get_inventory():add_item('main', 'lottarmor:chestplate_mithril')
+	elseif chance == 4 then
+		player:get_inventory():add_item('main', 'lottarmor:chestplate_tin')
+	elseif chance == 5 then
+		player:get_inventory():add_item('main', 'lottarmor:chestplate_galvorn')
+	elseif chance == 6 then
+		player:get_inventory():add_item('main', 'lottarmor:chestplate_gold')
+	end
 	player:get_inventory():add_item('main', 'lottinventory:crafts_book')
 end
 
 local function give_stuff_elf(player)
-	player:get_inventory():add_item('main', 'default:pick_steel')
-	player:get_inventory():add_item('main', 'default:axe_steel')
-	player:get_inventory():add_item('main', 'default:shovel_steel')
-	player:get_inventory():add_item('main', 'lottthrowing:bow_wood_mallorn')
-	player:get_inventory():add_item('main', 'lottthrowing:arrow 99')
-	player:get_inventory():add_item('main', 'lottinventory:potions_book')
+	regen_chance()
+	if chance <= 2 then
+		player:get_inventory():add_item('main', 'default:pick_steel')
+	elseif chance == 3 or chance == 4 or chance == 5 then
+		player:get_inventory():add_item('main', 'lottores:copperpick')
+	elseif chance == 6 then
+		player:get_inventory():add_item('main', 'lottores:galvornpick')
+	end
+	regen_chance()
+	if chance == 1 then
+		player:get_inventory():add_item('main', 'default:axe_steel')
+	elseif chance == 2 or chance == 3 then
+		player:get_inventory():add_item('main', 'lottores:tinaxe')
+	elseif chance == 4 then
+		player:get_inventory():add_item('main', 'lottores:galvornaxe')
+	elseif chance >= 5 then
+		player:get_inventory():add_item('main', 'lottores:copperaxe')
+	end
+	regen_chance()
+	if chance <= 3 then
+		player:get_inventory():add_item('main', 'lottores:coppershovel')
+	elseif chance >= 4 then
+		player:get_inventory():add_item('main', 'default:shovel_steel')
+	end
+	regen_chance()
+	if chance == 1 then
+		player:get_inventory():add_item('main', 'lottthrowing:bow_wood_alder')
+	elseif chance == 2 then
+		player:get_inventory():add_item('main', 'lottthrowing:bow_wood_lebethron')
+	elseif chance == 3 or chance == 4 then
+		player:get_inventory():add_item('main', 'lottthrowing:bow_wood')
+	elseif chance == 5 then
+		player:get_inventory():add_item('main', 'lottthrowing:bow_wood_mallorn')
+	elseif chance == 6 then
+		player:get_inventory():add_item('main', 'lottthrowing:bow_wood_birch')
+	end
+	regen_chance()
+	if chance <= 2 then
+		player:get_inventory():add_item('main', 'lottthrowing:arrow 99')
+	elseif chance == 3 or chance == 4 then
+		player:get_inventory():add_item('main', 'lottthrowing:arrow 50')
+	elseif chance >= 5 then
+		player:get_inventory():add_item('main', 'lottthrowing:arrow 25')
+	end
+	regen_chance()
+	if chance < 5 then
+		player:get_inventory():add_item('main', 'lottinventory:potions_book')
+	end
 	player:get_inventory():add_item('main', 'lottinventory:crafts_book')
 end
 
 local function give_stuff_man(player)
-	player:get_inventory():add_item('main', 'default:pick_steel')
-	player:get_inventory():add_item('main', 'default:axe_steel')
-	player:get_inventory():add_item('main', 'default:shovel_steel')
-	player:get_inventory():add_item('main', 'default:cobble 99')
-	player:get_inventory():add_item('main', 'default:stonebrick 99')
-	player:get_inventory():add_item('main', 'lottmobs:horseh1')
+	regen_chance()
+	if chance <= 2 then
+		player:get_inventory():add_item('main', 'default:pick_steel')
+	elseif chance == 3 or chance == 4 then
+		player:get_inventory():add_item('main', 'default:pick_bronze')
+	elseif chance == 5 then
+		player:get_inventory():add_item('main', 'lottores:tinpick')
+	elseif chance == 6 then
+		player:get_inventory():add_item('main', 'lottores:copperpick')
+	end
+	regen_chance()
+	if chance <= 3 then
+		player:get_inventory():add_item('main', 'lottores:silveraxe')
+	elseif chance == 4 then
+		player:get_inventory():add_item('main', 'default:axe_bronze')
+	elseif chance >= 5 then
+		player:get_inventory():add_item('main', 'default:axe_steel')
+	end
+	regen_chance()
+	if chance <= 2 then
+		player:get_inventory():add_item('main', 'default:shovel_stone')
+	elseif chance == 3 or chance == 4 then
+		player:get_inventory():add_item('main', 'default:shovel_steel')
+	elseif chance >= 5 then
+		player:get_inventory():add_item('main', 'lottores:tinshovel')
+	end
+	regen_chance()
+	if chance <= 2 then
+		player:get_inventory():add_item('main', 'default:cobble 99')
+		player:get_inventory():add_item('main', 'default:stonebrick 99')
+	elseif chance == 3 or chance == 4 then
+		player:get_inventory():add_item('main', 'default:desert_cobble 99')
+		player:get_inventory():add_item('main', 'default:desert_stonebrick 99')
+	elseif chance >= 5 then
+		player:get_inventory():add_item('main', 'default:sandstone 99')
+		player:get_inventory():add_item('main', 'default:sandstonebrick 99')
+	end
+	regen_chance()
+	if chance <= 2 then
+		player:get_inventory():add_item('main', 'lottmobs:horseh1')
+	elseif chance == 3 or chance == 4 then
+		player:get_inventory():add_item('main', 'lottmobs:horsepegh1')
+	elseif chance >= 5 then
+		player:get_inventory():add_item('main', 'lottmobs:horsearah1')
+	end
 	player:get_inventory():add_item('main', 'lottinventory:crafts_book')
 end
 
 local function give_stuff_orc(player)
-   player:get_inventory():add_item('main', 'lottweapons:orc_sword')
-   player:get_inventory():add_item('main', 'default:axe_steel')
-   player:get_inventory():add_item('main', 'default:shovel_steel')
-   player:get_inventory():add_item('main', 'default:cobble 99')
-   player:get_inventory():add_item('main', 'lottblocks:orc_brick 99')
-   player:get_inventory():add_item('main', 'lottother:orc_torch 99')
-   player:get_inventory():add_item('main', 'lottinventory:crafts_book')
+	player:get_inventory():add_item('main', 'lottweapons:orc_sword')
+	regen_chance()
+	if chance <= 2 then
+		player:get_inventory():add_item('main', 'default:axe_steel')
+	elseif chance == 3 or chance == 4 then
+		player:get_inventory():add_item('main', 'lottores:tinaxe')
+	elseif chance >= 5 then
+		player:get_inventory():add_item('main', 'lottores:copperaxe')
+	end
+	regen_chance()
+	if chance <= 2 then
+		player:get_inventory():add_item('main', 'default:shovel_steel')
+	elseif chance == 3 or chance == 4 then
+		player:get_inventory():add_item('main', 'lottores:tinshovel')
+	elseif chance >= 5 then
+		player:get_inventory():add_item('main', 'lottores:coppershovel')
+	end
+	regen_chance()
+	player:get_inventory():add_item('main', 'lottblocks:orc_brick 99')
+	player:get_inventory():add_item('main', 'lottfarming:orc_food 5')
+	player:get_inventory():add_item('main', 'lottfarming:orc_medicine 3')
+	player:get_inventory():add_item('main', 'lottother:orc_torch 50')
+	player:get_inventory():add_item('main', 'lottmobs:meat_raw 10')
+	player:get_inventory():add_item('main', 'lottinventory:crafts_book')
 end
 
 local function give_stuff_hobbit(player)
-	player:get_inventory():add_item('main', 'default:pick_stone')
-	player:get_inventory():add_item('main', 'default:axe_stone')
-	player:get_inventory():add_item('main', 'default:shovel_steel')
-	player:get_inventory():add_item('main', 'farming:hoe_bronze')
-	player:get_inventory():add_item('main', 'lottfarming:pipeweed_seed 14')
-	player:get_inventory():add_item('main', 'lottfarming:tomatoes_seed 21')
-	player:get_inventory():add_item('main', 'lottfarming:potato_seed 5')
+	regen_chance()
+	if chance <= 3 then
+		player:get_inventory():add_item('main', 'default:pick_stone')
+	elseif chance == 4 then
+		player:get_inventory():add_item('main', 'default:pick_wood')
+	elseif chance >= 5 then
+		player:get_inventory():add_item('main', 'default:pick_steel')
+	end
+	regen_chance()
+	player:get_inventory():add_item('main', 'default:axe_steel')
+	if chance <= 2 then
+		player:get_inventory():add_item('main', 'default:shovel_steel')
+	elseif chance == 3 or chance == 4 then
+		player:get_inventory():add_item('main', 'default:shovel_bronze')
+	elseif chance >= 5 then
+		player:get_inventory():add_item('main', 'lottores:silvershovel')
+	end
+	regen_chance()
+	if chance <= 3 then
+		player:get_inventory():add_item('main', 'farming:hoe_bronze')
+	elseif chance >= 4 then
+		player:get_inventory():add_item('main', 'farming:hoe_steel')
+	end
+	regen_chance()
 	player:get_inventory():add_item('main', 'lottfarming:pipe')
-	player:get_inventory():add_item('main', 'lottfarming:pipeweed_cooked')
-	player:get_inventory():add_item('main', 'lottinventory:cooking_book')
-	player:get_inventory():add_item('main', 'lottinventory:brewing_book')
+	player:get_inventory():add_item('main', 'lottfarming:pipeweed_cooked ' .. math.random(2, 15))
+	player:get_inventory():add_item('main', 'lottfarming:pipeweed_seed ' .. math.random(5, 30))
+	player:get_inventory():add_item('main', 'lottfarming:tomatoes_seed ' .. math.random(5, 30))
+	player:get_inventory():add_item('main', 'lottfarming:potato_seed ' .. math.random(5, 30))
+	if chance <= 3 then
+		player:get_inventory():add_item('main', 'lottinventory:cooking_book')
+	elseif chance >= 4 then
+		player:get_inventory():add_item('main', 'lottinventory:brewing_book')
+	end
 	player:get_inventory():add_item('main', 'lottinventory:crafts_book')
 	player:get_inventory():add_item('main', 'lottinventory:small')
 end
