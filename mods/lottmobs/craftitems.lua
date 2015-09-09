4b7cf094150f2864e17f61da0a075a089cf3f7dd
Amaz <Amaz1@users.noreply.github.com>
2015-09-09 22:23:25 +0100

    Improve the drops. They still need tweaking a little here and there, but I think that they are a lot better than they were

---
+++ mods/lottmobs/craftitems.lua
@@ -0,0 +1,42 @@
+minetest.register_craftitem("lottmobs:meat", {
+	description = "Cooked Meat",
+	inventory_image = "lottmobs_meat.png",
+	on_use = minetest.item_eat(4),
+})
+
+minetest.register_craftitem("lottmobs:meat_raw", {
+	description = "Raw Meat",
+	inventory_image = "lottmobs_meat_raw.png",
+})
+
+minetest.register_craft({
+	type = "cooking",
+	output = "lottmobs:meat",
+	recipe = "lottmobs:meat_raw",
+})
+
+minetest.register_craftitem("lottmobs:dirty_shirt", {
+    description = "Dirty Jacket",
+    inventory_image = "lottclothes_inv_jacket_midgewater.png^[colorize:#935d3675",
+    stack_max = 1,
+})
+
+minetest.register_craftitem("lottmobs:dirty_trousers", {
+    description = "Dirty Trousers",
+    inventory_image = "lottclothes_inv_pants_midgewater.png^[colorize:#935d3675",
+    stack_max = 1,
+})
+
+minetest.register_craft({
+    output = "lottclothes:jacket_midgewater",
+    type = "shapeless",
+    recipe = {"bucket:bucket_water", "lottmobs:dirty_shirt"},
+    replacements = {{"bucket:bucket_water", "bucket:bucket_empty"}},
+})
+
+minetest.register_craft({
+    output = "lottclothes:pants_midgewater",
+    type = "shapeless",
+    recipe = {"bucket:bucket_water", "lottmobs:dirty_trousers"},
+    replacements = {{"bucket:bucket_water", "bucket:bucket_empty"}},
+})
