221fb0127c56e857c9aefb91cdbdc7f3ca37c339
Andreas Gwilt <andreas@gwilt.net>
2015-11-18 11:32:54 +0100

    Add crafting recipes

--- mods/lottclothes/wizard.lua
+++ mods/lottclothes/wizard.lua
@@ -26,12 +26,28 @@ minetest.register_tool("lottclothes:cloak_wizard_grey", {
 	wear = 0
 })
 
+minetest.register_craft({
+	output = "lottclothes:cloak_wizard_grey",
+	recipe = {
+		{"lottclothes:felt_grey", "lottclothes:felt_grey", "lottclothes:felt_grey"},
+		{"lottclothes:felt_grey", "lottclothes:felt_grey", "lottclothes:felt_grey"}
+	}
+})
+
 -- hood
 minetest.register_tool("lottclothes:hood_wizard_blue", {
 	description = "Blue Wizard Hood",
 	inventory_image = "lottclothes_inv_hood_wizard_blue.png",
 	groups = {armor_heal=0, clothes=1, no_preview=1, clothes_head=1},
 	wear = 0
+})
+
+minetest.register_craft({
+	output = "lottclothes:hood_wizard_blue",
+	recipe = {
+		{"lottclothes:felt_blue", "lottclothes:felt_blue", "lottclothes:felt_blue"},
+		{"lottclothes:felt_blue", "", "lottclothes:felt_blue"}
+	}
 })
 
 -- robe
@@ -42,6 +58,22 @@ minetest.register_tool("lottclothes:robe_wizard_grey", {
 	wear = 0
 })
 
+minetest.register_craft({
+	output = "lottclothes:robe_wizard_grey",
+	recipe = {
+		{"lottclothes:felt_grey", "", "lottclothes:felt_grey"},
+		{"lottclothes:felt_grey", "lottclothes:felt_grey", "lottclothes:felt_grey"},
+		{"lottclothes:felt_grey", "lottclothes:felt_grey", "lottclothes:felt_grey"}
+	}
+})
+
+
+
+
+
+
+
+
 -- gandalf the white
 -- hood
 minetest.register_tool("lottclothes:hood_wizard_white", {
@@ -49,6 +81,14 @@ minetest.register_tool("lottclothes:hood_wizard_white", {
 	inventory_image = "lottclothes_inv_hood_wizard_white.png",
 	groups = {armor_heal=0, clothes=1, no_preview=1, clothes_head=1},
 	wear = 0
+})
+
+minetest.register_craft({
+	output = "lottclothes:hood_wizard_white",
+	recipe = {
+		{"lottclothes:felt_white", "lottclothes:felt_white", "lottclothes:felt_white"},
+		{"lottclothes:felt_white", "", "lottclothes:felt_white"}
+	}
 })
 
 -- robe
@@ -59,6 +99,15 @@ minetest.register_tool("lottclothes:robe_wizard_white", {
 	wear = 0
 })
 
+minetest.register_craft({
+	output = "lottclothes:robe_wizard_white",
+	recipe = {
+		{"lottclothes:felt_white", "", "lottclothes:felt_white"},
+		{"lottclothes:felt_white", "lottclothes:felt_white", "lottclothes:felt_white"},
+		{"lottclothes:felt_white", "lottclothes:felt_white", "lottclothes:felt_white"}
+	}
+})
+
 -- trousers
 minetest.register_tool("lottclothes:trousers_wizard_white", {
 	description = "White Wizard Trousers",
@@ -67,10 +116,28 @@ minetest.register_tool("lottclothes:trousers_wizard_white", {
 	wear = 0
 })
 
+minetest.register_craft({
+	output = "lottclothes:trousers_wizard_white",
+	recipe = {
+		{"lottclothes:felt_white", "lottclothes:felt_white", "lottclothes:felt_white"},
+		{"lottclothes:felt_white", "", "lottclothes:felt_white"},
+		{"lottclothes:felt_white", "", "lottclothes:felt_white"}
+	}
+})
+
 -- cloak
 minetest.register_tool("lottclothes:cloak_wizard_white", {
 	description = "White Wizard Cloak",
 	inventory_image = "lottclothes_inv_cloak_wizard_white.png",
 	groups = {armor_heal=0, clothes=1, no_preview=1, clothes_cloak=1},
 	wear = 0
+})
+
+minetest.register_craft({
+	output = "lottclothes:cloak_wizard_white",
+	recipe = {
+		{"lottclothes:felt_white", "lottclothes:felt_white"},
+		{"lottclothes:felt_white", "lottclothes:felt_white"},
+		{"lottclothes:felt_white", "lottclothes:felt_white"}
+	}
 })
