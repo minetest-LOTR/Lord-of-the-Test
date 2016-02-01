6d5e8a6613325e6b67a342dc38ba3a6bc1e4e004
badtoad2000 <jameson.miller@outlook.com>
2016-02-01 13:47:23 -0600

    Adding a comment so I can give the branch a meaningful name

--- mods/default/crafting.lua
+++ mods/default/crafting.lua
@@ -322,6 +322,7 @@ minetest.register_craft({
 	}
 })
 
+-- Making sandstone easier to craft. 4 Sand --> 2 Sandstone, so later 1 Sandstone --> 2 Sand
 minetest.register_craft({
 	output = 'default:sandstone 2',
 	recipe = {
