30e76dcdd72b40aff5849c487323069d4b26f78d
badtoad2000 <jameson.miller@outlook.com>
2016-02-05 23:38:49 -0600

    Change registration of wooden items so doors, trapdoors, fence, tables and chairs are no longer of the "wood" group, as that indicates they are planks. Being in group "wood" means they could be used in crafting recipes where planks would normally go.
    Change fences crafted from woods other than default, to use the same groups as the wood type they are crafted from.
    
    Remove duplicate node registration code from beds.lua.

--- mods/lottblocks/wooden_stuff.lua
+++ mods/lottblocks/wooden_stuff.lua
@@ -1,42 +1,50 @@
 function lottblocks.register_wooden_stuff(name, description, texture, wood_name)
-    local node_groups = minetest.registered_nodes[wood_name].groups
-    local groups_door = node_groups
-    groups_door.door = 1
+	local wood_groups = minetest.registered_nodes[wood_name].groups
+	local node_groups = {}
+	for i,v in ipairs(wood_groups) do
+		if i ~= "wood" then
+			node_groups[i] = v
+		end
+	end
+
+	local groups_door = node_groups
+	groups_door.door = 1
+
 	if name ~= "wood" then
 		doors:register_door("lottblocks:door_" .. name, {
-        	description =  description .. " Door",
-        	inventory_image = "lottblocks_door_" .. name .. ".png",
-        	groups = groups_door,
-        	tiles_bottom = {"lottblocks_door_" .. name .."_b.png", "lottblocks_edge_" .. name ..".png"},
-        	tiles_top = {"lottblocks_door_" .. name .. "_a.png", "lottblocks_edge_" .. name ..".png"},
-        })
-        minetest.register_craft({
-        	output = "lottblocks:door_" .. name,
-        	recipe = {
-        		{wood_name, wood_name},
-        		{wood_name, wood_name},
-        		{wood_name, wood_name}
-        	}
-        })
+        		description =  description .. " Door",
+        		inventory_image = "lottblocks_door_" .. name .. ".png",
+        		groups = groups_door,
+			tiles_bottom = {"lottblocks_door_" .. name .."_b.png", "lottblocks_edge_" .. name ..".png"},
+			tiles_top = {"lottblocks_door_" .. name .. "_a.png", "lottblocks_edge_" .. name ..".png"},
+		})
+		minetest.register_craft({
+			output = "lottblocks:door_" .. name,
+			recipe = {
+				{wood_name, wood_name},
+				{wood_name, wood_name},
+				{wood_name, wood_name}
+			}
+		})
 		node_groups.not_in_creative_inventory = 0
 		doors.register_trapdoor("lottblocks:hatch_" .. name, {
-        	description = description .. " Trapdoor",
-        	wield_image = "lottblocks_hatch_" .. name ..".png",
-        	tile_open = "lottblocks_hatch_" .. name .. "_open.png",
-        	tile_closed = "lottblocks_hatch_" .. name .. ".png",
-        	tile_side = "door_trapdoor_side.png",
-        	groups = node_groups,
-        	sounds = default.node_sound_wood_defaults(),
-        	sound_open = "door_door_open",
-        	sound_close = "door_door_close"
-        })
-        minetest.register_craft({
-        	output = "lottblocks:hatch_" .. name,
-        	recipe = {
-        		{wood_name, wood_name},
-        		{wood_name, wood_name},
-        	}
-        })
+			description = description .. " Trapdoor",
+			wield_image = "lottblocks_hatch_" .. name ..".png",
+			tile_open = "lottblocks_hatch_" .. name .. "_open.png",
+			tile_closed = "lottblocks_hatch_" .. name .. ".png",
+			tile_side = "door_trapdoor_side.png",
+			groups = node_groups,
+			sounds = default.node_sound_wood_defaults(),
+			sound_open = "door_door_open",
+			sound_close = "door_door_close"
+		})
+		minetest.register_craft({
+			output = "lottblocks:hatch_" .. name,
+			recipe = {
+				{wood_name, wood_name},
+				{wood_name, wood_name},
+			}
+		})
 		minetest.register_node("lottblocks:fence_" .. name, {
 			description = description .. " Fence",
 			drawtype = "fencelike",
@@ -49,7 +57,7 @@ function lottblocks.register_wooden_stuff(name, description, texture, wood_name)
 				type = "fixed",
 				fixed = {-1/7, -1/2, -1/7, 1/7, 1/2, 1/7},
 			},
-			groups = {choppy=2,oddly_breakable_by_hand=2,flammable=2},
+			groups = node_groups
 		})
 		minetest.register_craft({
 			output = "lottblocks:fence_" .. name .." 6",
@@ -58,70 +66,70 @@ function lottblocks.register_wooden_stuff(name, description, texture, wood_name)
 				{wood_name, wood_name, wood_name,},
 			}
 		})
-    end
-    minetest.register_node("lottblocks:" .. name .. "_table", {
-        description = description .. "Table",
-    	tiles = {texture},
-    	drawtype = "nodebox",
-    	sunlight_propagates = true,
-    	paramtype = 'light',
-    	paramtype2 = "facedir",
-    	node_box = {
-    		type = "fixed",
-    		fixed = {
-    			{-0.4,-0.5,-0.4, -0.3,0.4,-0.3},
-    			{0.3,-0.5,-0.4, 0.4,0.4,-0.3},
-    			{-0.4,-0.5,0.3, -0.3,0.4,0.4},
-    			{0.3,-0.5,0.3, 0.4,0.4,0.4},
-    			{-0.5,0.4,-0.5, 0.5,0.5,0.5},
-    			{-0.4,-0.2,-0.3, -0.3,-0.1,0.3},
-    			{0.3,-0.2,-0.4, 0.4,-0.1,0.3},
-    			{-0.3,-0.2,-0.4, 0.4,-0.1,-0.3},
-    			{-0.3,-0.2,0.3, 0.3,-0.1,0.4},
-    		},
-    	},
-    	groups = node_groups
-    })
-    minetest.register_node("lottblocks:" .. name .."_chair", {
-        description = description .. "Chair",
-    	tiles = {texture},
-    	drawtype = "nodebox",
-    	sunlight_propagates = true,
-    	paramtype = 'light',
-    	paramtype2 = "facedir",
-    	node_box = {
-    		type = "fixed",
-    		fixed = {
-    			{-0.3,-0.5,0.2, -0.2,0.5,0.3},
-    			{0.2,-0.5,0.2, 0.3,0.5,0.3},
-    			{-0.3,-0.5,-0.3, -0.2,-0.1,-0.2},
-    			{0.2,-0.5,-0.3, 0.3,-0.1,-0.2},
-    			{-0.3,-0.1,-0.3, 0.3,0,0.2},
-    			{-0.2,0.1,0.25, 0.2,0.4,0.26}
-    		},
-    	},
-    	selection_box = {
-    		type = "fixed",
-    		fixed = {-0.3, -0.5, -0.3, 0.3, 0.5, 0.3},
-    	},
-    	groups = node_groups
-    })
-    minetest.register_craft({
-    	output = "lottblocks:" .. name .. "_table",
-    	recipe = {
-    		{wood_name, wood_name, wood_name},
-    		{'group:stick', 'group:stick', 'group:stick'},
-    		{'group:stick', '', 'group:stick'},
-    	}
-    })
-    minetest.register_craft({
-    	output = "lottblocks:" .. name .. "_chair",
-    	recipe = {
-    		{'group:stick', ''},
-    		{wood_name, wood_name},
-    		{'group:stick', 'group:stick'},
-    	}
-    })
+	end
+	minetest.register_node("lottblocks:" .. name .. "_table", {
+		description = description .. "Table",
+		tiles = {texture},
+		drawtype = "nodebox",
+		sunlight_propagates = true,
+		paramtype = 'light',
+		paramtype2 = "facedir",
+		node_box = {
+			type = "fixed",
+			fixed = {
+				{-0.4,-0.5,-0.4, -0.3,0.4,-0.3},
+				{0.3,-0.5,-0.4, 0.4,0.4,-0.3},
+				{-0.4,-0.5,0.3, -0.3,0.4,0.4},
+				{0.3,-0.5,0.3, 0.4,0.4,0.4},
+				{-0.5,0.4,-0.5, 0.5,0.5,0.5},
+				{-0.4,-0.2,-0.3, -0.3,-0.1,0.3},
+				{0.3,-0.2,-0.4, 0.4,-0.1,0.3},
+				{-0.3,-0.2,-0.4, 0.4,-0.1,-0.3},
+				{-0.3,-0.2,0.3, 0.3,-0.1,0.4},
+			},
+		},
+		groups = node_groups
+	})
+	minetest.register_node("lottblocks:" .. name .."_chair", {
+		description = description .. "Chair",
+		tiles = {texture},
+		drawtype = "nodebox",
+		sunlight_propagates = true,
+		paramtype = 'light',
+		paramtype2 = "facedir",
+		node_box = {
+			type = "fixed",
+			fixed = {
+				{-0.3,-0.5,0.2, -0.2,0.5,0.3},
+				{0.2,-0.5,0.2, 0.3,0.5,0.3},
+				{-0.3,-0.5,-0.3, -0.2,-0.1,-0.2},
+				{0.2,-0.5,-0.3, 0.3,-0.1,-0.2},
+				{-0.3,-0.1,-0.3, 0.3,0,0.2},
+				{-0.2,0.1,0.25, 0.2,0.4,0.26}
+			},
+		},
+		selection_box = {
+			type = "fixed",
+			fixed = {-0.3, -0.5, -0.3, 0.3, 0.5, 0.3},
+		},
+		groups = node_groups
+	})
+	minetest.register_craft({
+		output = "lottblocks:" .. name .. "_table",
+		recipe = {
+			{wood_name, wood_name, wood_name},
+			{'group:stick', 'group:stick', 'group:stick'},
+			{'group:stick', '', 'group:stick'},
+		}
+	})
+	minetest.register_craft({
+		output = "lottblocks:" .. name .. "_chair",
+		recipe = {
+			{'group:stick', ''},
+			{wood_name, wood_name},
+			{'group:stick', 'group:stick'},
+		}
+	})
 end
 
 lottblocks.register_wooden_stuff("wood", "Wooden", "default_wood.png", "default:wood")
