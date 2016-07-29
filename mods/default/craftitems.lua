57107099b0bb7b038ae5f6435d3d6df5c593c974
Amaz <Amaz1@users.noreply.github.com>
2016-07-29 21:15:12 +0100

    Update books

--- mods/default/craftitems.lua
+++ mods/default/craftitems.lua
@@ -11,81 +11,135 @@ minetest.register_craftitem("default:paper", {
 	inventory_image = "default_paper.png",
 })
 
-local function book_on_use(itemstack, user, pointed_thing)
+local lpp = 14 -- Lines per book's page
+local function book_on_use(itemstack, user)
 	local player_name = user:get_player_name()
 	local data = minetest.deserialize(itemstack:get_metadata())
 	local title, text, owner = "", "", player_name
+	local page, page_max, lines, string = 1, 1, {}, ""
+
 	if data then
-		title, text, owner = data.title, data.text, data.owner
+		title = data.title
+		text = data.text
+		owner = data.owner
+
+		for str in (text .. "\n"):gmatch("([^\n]*)[\n]") do
+			lines[#lines+1] = str
+		end
+
+		if data.page then
+			page = data.page
+			page_max = data.page_max
+
+			for i = ((lpp * page) - lpp) + 1, lpp * page do
+				if not lines[i] then break end
+				string = string .. lines[i] .. "\n"
+			end
+		end
 	end
+
 	local formspec
 	if owner == player_name then
-		formspec = "size[8,8]"..
-			"background[8,8;1,1;craft_formbg.png;true]"..
-			"field[0.5,1;7.5,0;title;Title:;"..
-				minetest.formspec_escape(title).."]"..
-			"textarea[0.5,1.5;7.5,7;text;Contents:;"..
-				minetest.formspec_escape(text).."]"..
+		formspec = "size[8,8]" ..
+			"bgcolor[#a0a0a0;false]"..
+			"background[8,8;1,1;gui_frame.png;true]"..
+			"field[0.5,1;7.5,0;title;Title:;" ..
+				minetest.formspec_escape(title) .. "]" ..
+			"textarea[0.5,1.5;7.5,7;text;Contents:;" ..
+				minetest.formspec_escape(text) .. "]" ..
 			"button_exit[2.5,7.5;3,1;save;Save]"
 	else
-		formspec = "size[8,8]"..
-			"background[8,8;1,1;craft_formbg.png;true]"..
-			"label[0.5,0.5;by "..owner.."]"..
-			"label[0.5,0;"..minetest.formspec_escape(title).."]"..
-			"tableoptions[background=#00000000;highlight=#00000000;border=false]"..
-			"table[0.5,1.5;7.5,7;;"..minetest.formspec_escape(text):gsub("\n", ",")..";1]"
+		formspec = "size[8,8]" ..
+			"background[8,8;1,1;gui_frame.png;true]"..
+			"bgcolor[#a0a0a0;false]"..
+			"label[0.5,0.5;by " .. owner .. "]" ..
+			"tablecolumns[color;text]" ..
+			"tableoptions[background=#00000000;highlight=#00000000;border=false]" ..
+			"table[0.4,0;7,0.5;title;#FFFF00," .. minetest.formspec_escape(title) .. "]" ..
+			"textarea[0.5,1.5;7.5,7;;" ..
+			minetest.formspec_escape(string ~= "" and string or text) .. ";]" ..
+			"button[2.4,7.6;0.8,0.8;book_prev;<]" ..
+			"label[3.2,7.7;Page " .. page .. " of " .. page_max .. "]" ..
+			"button[4.9,7.6;0.8,0.8;book_next;>]"
 	end
-	minetest.show_formspec(user:get_player_name(), "default:book", formspec)
+
+	minetest.show_formspec(player_name, "default:book", formspec)
 end
 
-minetest.register_on_player_receive_fields(function(player, form_name, fields)
-	if form_name ~= "default:book" or not fields.save or
-			fields.title == "" or fields.text == "" then
-		return
-	end
+minetest.register_on_player_receive_fields(function(player, formname, fields)
+	if formname ~= "default:book" then return end
 	local inv = player:get_inventory()
 	local stack = player:get_wielded_item()
-	local new_stack, data
-	if stack:get_name() ~= "default:book_written" then
-		local count = stack:get_count()
-		if count == 1 then
-			stack:set_name("default:book_written")
+
+	if fields.save and fields.title ~= "" and fields.text ~= "" then
+		local new_stack, data
+		if stack:get_name() ~= "default:book_written" then
+			local count = stack:get_count()
+			if count == 1 then
+				stack:set_name("default:book_written")
+			else
+				stack:set_count(count - 1)
+				new_stack = ItemStack("default:book_written")
+			end
 		else
-			stack:set_count(count - 1)
-			new_stack = ItemStack("default:book_written")
+			data = minetest.deserialize(stack:get_metadata())
 		end
-	else
-		data = minetest.deserialize(stack:get_metadata())
-	end
-	if not data then data = {} end
-	data.title = fields.title
-	data.text = fields.text
-	data.owner = player:get_player_name()
-	local data_str = minetest.serialize(data)
-	if new_stack then
-		new_stack:set_metadata(data_str)
-		if inv:room_for_item("main", new_stack) then
-			inv:add_item("main", new_stack)
+
+		if not data then data = {} end
+		data.title = fields.title
+		data.text = fields.text
+		data.text_len = #data.text
+		data.page = 1
+		data.page_max = math.ceil((#data.text:gsub("[^\n]", "") + 1) / lpp)
+		data.owner = player:get_player_name()
+		local data_str = minetest.serialize(data)
+
+		if new_stack then
+			new_stack:set_metadata(data_str)
+			if inv:room_for_item("main", new_stack) then
+				inv:add_item("main", new_stack)
+			else
+				minetest.add_item(player:getpos(), new_stack)
+			end
 		else
-			minetest.add_item(player:getpos(), new_stack)
+			stack:set_metadata(data_str)
 		end
-	else
+
+	elseif fields.book_next or fields.book_prev then
+		local data = minetest.deserialize(stack:get_metadata())
+		if not data.page then return end
+
+		if fields.book_next then
+			data.page = data.page + 1
+			if data.page > data.page_max then
+				data.page = 1
+			end
+		else
+			data.page = data.page - 1
+			if data.page == 0 then
+				data.page = data.page_max
+			end
+		end
+
+		local data_str = minetest.serialize(data)
 		stack:set_metadata(data_str)
+		book_on_use(stack, player)
 	end
+
 	player:set_wielded_item(stack)
 end)
 
 minetest.register_craftitem("default:book", {
 	description = "Book",
 	inventory_image = "default_book.png",
-	groups = {book=1},
+	groups = {book = 1},
 	on_use = book_on_use,
 })
 
 minetest.register_craftitem("default:book_written", {
 	description = "Book With Text",
 	inventory_image = "default_book_written.png",
-	groups = {book=1, not_in_creative_inventory=1},
+	groups = {book = 1, not_in_creative_inventory = 1},
 	stack_max = 1,
 	on_use = book_on_use,
 })
@@ -93,7 +147,7 @@ minetest.register_craftitem("default:book_written", {
 minetest.register_craft({
 	type = "shapeless",
 	output = "default:book_written",
-	recipe = { "default:book", "default:book_written" }
+	recipe = {"default:book", "default:book_written"}
 })
 
 minetest.register_on_craft(function(itemstack, player, old_craft_grid, craft_inv)
@@ -101,7 +155,6 @@ minetest.register_on_craft(function(itemstack, player, old_craft_grid, craft_inv
 		return
 	end
 
-	local copy = ItemStack("default:book_written")
 	local original
 	local index
 	for i = 1, player:get_inventory():get_size("craft") do
