b627c25831f8efcdacab44a9e15c536097fe4de3
Lumidify Productions <lumidify@openmailbox.org>
2016-08-16 07:31:05 +0200

    Remove hobbits with full mithril armor
    Might still need some tweaking

--- mods/lottmobs/hobbits.lua
+++ mods/lottmobs/hobbits.lua
@@ -64,16 +64,16 @@ local textures1 = {
 
 lottmobs.register_hobbit("", 20, 30, textures1, 2, 4, 5, 175, drops, 20)
 
---Dwarfs with mithril armor & warhammers.
+--Hobbits with mithril or galvorn armor and mithril daggers.
 local textures2 = {
-    {"lottmobs_hobbit_1.png", "lottarmor_helmet_mithril.png^lottarmor_chestplate_mithril.png^lottarmor_leggings_mithril.png^lottarmor_boots_mithril.png^lottarmor_shield_mithril.png", "lottweapons_mithril_warhammer.png", "lottarmor_trans.png"},
-    {"lottmobs_hobbit_2.png", "lottarmor_helmet_mithril.png^lottarmor_chestplate_mithril.png^lottarmor_leggings_mithril.png^lottarmor_boots_mithril.png^lottarmor_shield_mithril.png", "lottweapons_mithril_warhammer.png", "lottarmor_trans.png"},
-    {"lottmobs_hobbit_3.png", "lottarmor_helmet_mithril.png^lottarmor_chestplate_mithril.png^lottarmor_leggings_mithril.png^lottarmor_boots_mithril.png^lottarmor_shield_mithril.png", "lottweapons_mithril_warhammer.png", "lottarmor_trans.png"},
+    {"lottmobs_hobbit_1.png", "lottarmor_chestplate_galvorn.png", "lottweapons_mithril_dagger.png", "lottarmor_trans.png"},
+    {"lottmobs_hobbit_2.png", "lottarmor_chestplate_mithril.png", "lottweapons_mithril_dagger.png", "lottarmor_trans.png"},
+    {"lottmobs_hobbit_3.png", "lottarmor_chestplate_mithril.png^lottarmor_shield_galvorn.png", "lottweapons_mithril_dagger.png", "lottarmor_trans.png"},
 }
 
-lottmobs.register_hobbit("1", 30, 40, textures2, 2.75, 3.5, 13, 75, drops, 60)
+lottmobs.register_hobbit("1", 30, 40, textures2, 2.75, 3.5, 9, 75, drops, 60)
 
---Dwarfs with steel armor & swords.
+--Hobbits with steel armor & swords.
 local textures3 = {
     {"lottmobs_hobbit_1.png", "lottarmor_helmet_steel.png^lottarmor_chestplate_steel.png^lottarmor_leggings_steel.png^lottarmor_boots_steel.png^lottarmor_shield_steel.png", "default_tool_steelsword.png", "lottarmor_trans.png"},
     {"lottmobs_hobbit_2.png", "lottarmor_helmet_steel.png^lottarmor_chestplate_steel.png^lottarmor_leggings_steel.png^lottarmor_boots_steel.png^lottarmor_shield_steel.png", "default_tool_steelsword.png", "lottarmor_trans.png"},
