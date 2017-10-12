66cb2b12a77284cd2bdc8268c706a80a61956f91
NewbProgrammer101 <countolaf17@gmail.com>
2017-10-11 23:16:05 -0500

    Update wooden_stuff.lua

--- mods/lottblocks/wooden_stuff.lua
+++ mods/lottblocks/wooden_stuff.lua
@@ -163,24 +163,120 @@ function lottblocks.register_wooden_stuff(name, description, texture, wood_name)
 		tiles = {texture},
 		drawtype = "nodebox",
 		sunlight_propagates = true,
-		paramtype = 'light',
+		paramtype = "light",
 		paramtype2 = "facedir",
+		collision_box={
+			type="fixed",
+			fixed={
+				{-0.3125, -0.5, -0.3125, 0.3125, -0.0625, 0.3125},
+				{-0.3125, -0.5, 0.1875, -0.1875, 0.5, 0.3125},
+		},
 		node_box = {
 			type = "fixed",
 			fixed = {
-				{-0.3,-0.5,0.2, -0.2,0.5,0.3},
-				{0.2,-0.5,0.2, 0.3,0.5,0.3},
-				{-0.3,-0.5,-0.3, -0.2,-0.1,-0.2},
-				{0.2,-0.5,-0.3, 0.3,-0.1,-0.2},
-				{-0.3,-0.1,-0.3, 0.3,0,0.2},
-				{-0.2,0.1,0.25, 0.2,0.4,0.26}
-			},
+				{-0.3125, -0.5, 0.1875, -0.1875, 0.5, 0.3125},
+				{0.1875, -0.5, 0.1875, 0.3125, 0.5, 0.3125},
+				{0.1875, -0.5, -0.3125, 0.3125, -0.0625, -0.1875},
+				{-0.3125, -0.5, -0.3125, -0.1875, -0.0625, -0.1875},
+				{-0.3125, -0.125, -0.3125, 0.3125, 0, 0.3125},
+				{-0.1875, 0.3125, 0.1875, 0.1875, 0.4375, 0.3125},
+				{-0.3125, 0.125, 0.1875, 0.3125, 0.1875, 0.3125},
+				{0.23, -0.4375, -0.3125, 0.29, -0.375, 0.3125},
+				{-0.29, -0.4375, -0.3125, -0.23, -0.375, 0.3125},
+				{-0.29, -0.4375, -0.0315, 0.29, -0.375, 0.031},
+			}
 		},
 		selection_box = {
 			type = "fixed",
-			fixed = {-0.3, -0.5, -0.3, 0.3, 0.5, 0.3},
+			fixed = {-0.3125, -0.5, -0.3125, 0.3125, 0.5, 0.3125},
 		},
-		groups = node_groups
+		groups = {node_groups, oddly_breakable_by_hand = 2, flammable = 1},
+		on_rightclick = function(pos, node, player, itemstack, pointed_thing)
+			local v=player:get_player_velocity()
+			if v.x~=0 or v.y~=0 or v.z~=0 then return end
+			player:setpos({x=pos.x,y=pos.y,z=pos.z})
+			local name=player:get_player_name()
+			local nname=minetest.get_node(pos).name
+			if default.player_attached[name] then
+				player:set_physics_override(1, 1, 1)
+				minetest.after(0.3, function(player,name)
+					player:set_eye_offset({x=0,y=0,z=0}, {x=0,y=0,z=0})
+					default.player_attached[name]=false
+					default.player_set_animation(player, "stand",30)
+				end,player,name)
+			else
+				player:set_physics_override(0, 0, 0)
+				minetest.after(0.3, function(player,name)
+					player:set_eye_offset({x=0,y=-7,z=2}, {x=0,y=0,z=0})
+					default.player_attached[name]=true
+					default.player_set_animation(player, "sit",30)
+				end,player,name)
+				minetest.after(0.3, function(player,name)
+					player:set_eye_offset({x=0,y=-7,z=2}, {x=0,y=0,z=0})
+					default.player_attached[name]=true
+					default.player_set_animation(player, "sit",30)
+				end,player,name)
+			end,
+		end,
+		can_dig = function(pos, player)
+			for _, ob in ipairs(minetest.get_objects_inside_radius(pos,1)) do
+				return false
+			end
+			return true
+		end,
+		on_construct=function(pos)
+			local meta=minetest.get_meta(pos)
+			minetest.get_node_timer(pos):start(1)
+			meta:set_int("n",20)
+			meta:set_int("y",0)
+		end,
+		on_timer = function (pos, elapsed)
+			local meta=minetest.get_meta(pos)
+			if meta:get_int("placed")==1 then
+				meta:set_int("n",0)
+				return
+			end
+			local n=meta:get_int("n")
+			meta:set_int("n",n-1)
+			if aliveai.def({x=pos.x+1,y=pos.y,z=pos.z},"buildable_to") then
+				local p,d=aliveai.xz_to_param2yaw(-1)
+				minetest.swap_node(pos, {name = "aliveai:chair", param2=p})
+				meta:set_int("y",d)
+			elseif aliveai.def({x=pos.x-1,y=pos.y,z=pos.z},"buildable_to") then
+				local p,d=aliveai.xz_to_param2yaw(1)
+				minetest.swap_node(pos, {name = "aliveai:chair", param2=p})
+				meta:set_int("y",d)
+			elseif aliveai.def({x=pos.x,y=pos.y,z=pos.z+1},"buildable_to") then
+				local p,d=aliveai.xz_to_param2yaw(0,-1)
+				minetest.swap_node(pos, {name = "aliveai:chair", param2=p})
+				meta:set_int("y",d)
+			elseif aliveai.def({x=pos.x,y=pos.y,z=pos.z-1},"buildable_to") then
+				local p,d=aliveai.xz_to_param2yaw(0,1)
+				minetest.swap_node(pos, {name = "aliveai:chair", param2=p})
+				meta:set_int("y",d)
+			end
+			if n<1 then
+				meta:set_int("n",0)
+				return
+			end
+			return true
+		end,
+			on_blast=function(pos)
+			for _, player in ipairs(minetest.get_objects_inside_radius(pos,1)) do
+				if player:is_player() then
+				local name=player:get_player_name()
+				player:set_physics_override(1, 1, 1)
+				minetest.after(0.3, function(player,name)
+					player:set_eye_offset({x=0,y=0,z=0}, {x=0,y=0,z=0})
+					default.player_attached[name]=false
+					default.player_set_animation(player, "stand",30)
+				end,player,name)
+				end
+			end
+		end,
+		after_place_node = function(pos, placer)
+			minetest.get_meta(pos):set_int("placed",1)
+		end
 	})
 	minetest.register_craft({
 		output = "lottblocks:" .. name .. "_table",
