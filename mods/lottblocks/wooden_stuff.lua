bf53df0dc02434c1123a134be547b1758c519cf1
NewbProgrammer101 <countolaf17@gmail.com>
2017-10-12 00:10:33 -0500

    Update wooden_stuff.lua

--- mods/lottblocks/wooden_stuff.lua
+++ mods/lottblocks/wooden_stuff.lua
@@ -184,6 +184,13 @@ function lottblocks.register_wooden_stuff(name, description, texture, wood_name)
 			type = "fixed",
 			fixed = {-0.3125, -0.5, -0.3125, 0.3125, 0.5, 0.3125},
 		},
+		collision_box={
+			type="fixed",
+			fixed={
+				{-0.3125, -0.5, -0.3125, 0.3125, -0.0625, 0.3125},
+				{-0.3125, -0.5, 0.1875, -0.1875, 0.5, 0.3125},
+			},
+		},
 		groups = node_groups,
 		on_rightclick = function(pos, node, player, itemstack, pointed_thing)
 			local v=player:get_player_velocity()
