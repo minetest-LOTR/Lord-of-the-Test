dddc923e469c8fcdefbbbdcf5041ae3618075957
NewbProgrammer101 <countolaf17@gmail.com>
2017-10-12 21:21:32 -0500

    Update init.lua

--- mods/lottblocks/init.lua
+++ mods/lottblocks/init.lua
@@ -1,8 +1,8 @@
 lottblocks = {}
-lottblocks.beds.player = {}
-lottblocks.beds.pos = {}
+lottblocks.player = {}
+lottblocks.pos = {}
 
-lottblocks.beds.formspec = "size[8,15;true]" ..
+lottblocks.formspec = "size[8,15;true]" ..
 	"bgcolor[#080808BB; true]" ..
   	"button_exit[2,12;4,0.75;leave;Leave Bed]"
 
