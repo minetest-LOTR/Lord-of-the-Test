fadebb59df9f330ffc1e93e7ba85b4b200e856e8
Jalus Bilieyich <dell30@debianmadman.austin.rr.com>
2017-10-27 18:48:52 -0500

    modified:   mods/lottblocks/guide_text/lore_poem1.lua

--- mods/lottblocks/guide_text/lore_poem1.lua
+++ mods/lottblocks/guide_text/lore_poem1.lua
@@ -1,39 +1,39 @@
 return "return {[\"background\"] = \"gui_elfbg.png\"," ..
 	"[\"title\" = \"A GILTHONIEL\", [\"page_max\"] = 5," ..
-	"[\text_len\"] = 2333, [\"owner\"] = \"A Wise Elf\", [\"text\"] = \The Men of the West " ..
-	"\\\n  Endowed with unique quests,
-	"\\\nAwoke Secondborn
-	"\\\n  In the morn,
-	"\\\nWith their vests
-	"\\\n  Filled with blesses
-	"\\\nOf Iluvatar the Creator
-	"\\\n  O! Beloved and Sterling!
-	"\\\n  A! Supreme of all things well!
-	"\\\n  O! Almighty of Varda!
-	"\\\n\\\n  Elbereth! Gilthoniel!
-	"\\\nQueen of the Stars.
-	"\\\n  Elbereth!
-	"\\\nSovereign of the Western Star,
-	"\\\nWith love of all things good,
-	"\\\nGuardian of whom call her name.
-	"\\\n  A! Elbereth Gilthoniel!
-	"\\\n\\\nClearest among all life,
-	"\\\nLife is in thy eyes,
-	"\\\nClear of all strife,
-	"\\\nLight of all faithful life, 
-	"\\\n  Elbereth! A Gilthoniel!
-	"\\\n\\\nThe Queen of stars
-	"\\\n  With thy admirers,
-	"\\\nBowing to thy feet
-	"\\\n  Lost in Thought and Song,
-	"\\\nBlazer of Evil in our paths
-	"\\\nAnd contributing with Light.
-	"\\\n  A! Elbereth!
-	"\\\n\\\nGilthoniel!
-	"\\\n\\\nO! Holy like pure gold
-	"\\\n  Much higher than those sold,
-	"\\\nWatcher of us over the Misty Mountains
-	"\\\n  And in the Shadow of Mordor,
-	"\\\nTremble we shall at the Song of thee.
-	"\\\n  Gilthoniel O Elbereth!
-	"\\\n\\\nFarewell and may Elbereth bring light to thee.
+	"[\text_len\"] = 2333, [\"owner\"] = \"A Wise Elf\", [\"text\"] = \"The Men of the West " ..
+	"\\\n  Endowed with unique quests, " ..
+	"\\\nAwoke Secondborn " ..
+	"\\\n  In the morn, " ..
+	"\\\nWith their vests " ..
+	"\\\n  Filled with blesses " ..
+	"\\\nOf Iluvatar the Creator " ..
+	"\\\n  O! Beloved and Sterling! " ..
+	"\\\n  A! Supreme of all things well! " ..
+	"\\\n  O! Almighty of Varda! " ..
+	"\\\n\\\n  Elbereth! Gilthoniel! " ..
+	"\\\nQueen of the Stars. " ..
+	"\\\n  Elbereth! " ..
+	"\\\nSovereign of the Western Star, " ..
+	"\\\nWith love of all things good, " ..
+	"\\\nGuardian of whom call her name. " ..
+	"\\\n  A! Elbereth Gilthoniel! " ..
+	"\\\n\\\nClearest among all life, " ..
+	"\\\nLife is in thy eyes, " ..
+	"\\\nClear of all strife, " ..
+	"\\\nLight of all faithful life, " ..
+	"\\\n  Elbereth! A Gilthoniel! " ..
+	"\\\n\\\nThe Queen of stars " ..
+	"\\\n  With thy admirers, " ..
+	"\\\nBowing to thy feet " ..
+	"\\\n  Lost in Thought and Song, " ..
+	"\\\nBlazer of Evil in our paths " ..
+	"\\\nAnd contributing with Light. " ..
+	"\\\n  A! Elbereth! " ..
+	"\\\n\\\nGilthoniel! " ..
+	"\\\n\\\nO! Holy like pure gold " ..
+	"\\\n  Much higher than those sold, " ..
+	"\\\nWatcher of us over the Misty Mountains " ..
+	"\\\n  And in the Shadow of Mordor, " ..
+	"\\\nTremble we shall at the Song of thee. " ..
+	"\\\n  Gilthoniel O Elbereth! " ..
+	"\\\n\\\nFarewell and may Elbereth bring light to thee. " ..
