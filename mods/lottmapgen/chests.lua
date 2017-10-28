4b78e760fb31466bec933c473cdbe53b6e081ee8
NewbProgrammer101 <countolaf17@gmail.com>
2017-10-27 19:13:29 -0500

    Update chests.lua

--- mods/lottmapgen/chests.lua
+++ mods/lottmapgen/chests.lua
@@ -290,8 +290,8 @@ minetest.register_abm({
 			[34]="lottthrowing:arrow_fire_blue",
 			[35]="lottthrowing:arrow_fire 3",
 			[36]="lottblocks:elf_torch 10",
-			[37]="lottores:white_gem",
-			[38] = "lottblocks:elven_rope " .. r(10, 40)
+			[37]="lottores:white_gem", .. r(10, 41)
+			[38] = "lottblocks:elven_rope "
 		}
 		local meta = minetest.get_meta(pos)
 		local inv = meta:get_inventory()
