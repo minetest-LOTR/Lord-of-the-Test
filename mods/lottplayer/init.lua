e417369a462bfb0945d28d17874082ea3a059e2a
Amaz <Amaz1@users.noreply.github.com>
2018-01-15 20:54:24 +0000

    Small bugfix

--- mods/lottplayer/init.lua
+++ mods/lottplayer/init.lua
@@ -278,11 +278,14 @@ end
 
 minetest.register_on_chat_message(function(name, message)
 	local player = minetest.get_player_by_name(name)
+	if not player then
+		return
+	end
 	local race = player:get_attribute("race")
 	if not race or race == "" then
 		return false
 	end
-	minetest.chat_send_all("<" .. minetest.colorize(races[tonumber(race)][7], name)
+	minetest.chat_send_all("<" .. minetest.colorize(lottplayer.races[tonumber(race)][7], name)
 		.. "> " .. message)
 	return true
 end)
