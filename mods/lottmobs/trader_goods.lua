12cde088a0f8f31774d2a6797b43e669c42f9b62
Isidor Zeuner <minetest@quidecco.de>
2018-09-24 18:49:52 +0200

    add translations

--- mods/lottmobs/trader_goods.lua
+++ mods/lottmobs/trader_goods.lua
@@ -1,3 +1,12 @@
+local MP = minetest.get_modpath(
+    minetest.get_current_modname(
+    )
+)
+
+local S, NS = dofile(
+    MP .. "/intllib.lua"
+)
+
 --Item table format:
 --{thing selling, price (in gold ingots), chance that it won't appear in the trader's inventory}
 
@@ -43,25 +52,25 @@ lottmobs.dwarf = {
 		"Ori", "Gimli", "Gamil"
 	},
 	messages = {
-		"We have many treasures, and for the right price we might be willing to part with them...",
-		"Don't even think of stealing our treasure... If you do, heads shall roll.",
-		"What are you doing here? What do you want from us?",
-		"Be careful when you enter our homes, a fall from the ladder could well prove deadly.",
-		"If you want to mine, do so. There's plenty of iron to go around!",
-		"If you venture deep underground, beware! The monsters there are very powerful, and kill the unprepared instantly.",
-		"Mines of Moria, where all our riches are! Mines of Moria, where our greed led to destruction.",
-		"We can’t bring a troll to the fire, but we can bring the fire to the troll.",
-		"Never turn down an ale, who knows if it may be your last...",
-		"Guard your life, guard your gold, guard your beard. In that order.",
-		"Wherever there are elves, there are lies.",
-		"The Humans have a saying. The nail that sticks out gets hammered. We have a saying too. Shoddy work! Not a single nail should be sticking out.",
-		"You can kill a dwarf, but you can never vanquish one.",
-		"It is easy to fool a goblin, but even easier to kill one.",
-		"The best place to hide something precious is in your beard.",
-		"Evil breeds in the guts of the lazy.",
-		"The stones will sing if you let them.",
-		"Drinking contests with humans are unbearable. They drink, they collapse, \nand we have to drag them back to their homes. The next day they never remember losing.",
-		"A spear is not a dwarven weapon, but it will kill all the same.",	
+		S("We have many treasures, and for the right price we might be willing to part with them..."),
+		S("Don't even think of stealing our treasure... If you do, heads shall roll."),
+		S("What are you doing here? What do you want from us?"),
+		S("Be careful when you enter our homes, a fall from the ladder could well prove deadly."),
+		S("If you want to mine, do so. There's plenty of iron to go around!"),
+		S("If you venture deep underground, beware! The monsters there are very powerful, and kill the unprepared instantly."),
+		S("Mines of Moria, where all our riches are! Mines of Moria, where our greed led to destruction."),
+		S("We can’t bring a troll to the fire, but we can bring the fire to the troll."),
+		S("Never turn down an ale, who knows if it may be your last..."),
+		S("Guard your life, guard your gold, guard your beard. In that order."),
+		S("Wherever there are elves, there are lies."),
+		S("The Humans have a saying. The nail that sticks out gets hammered. We have a saying too. Shoddy work! Not a single nail should be sticking out."),
+		S("You can kill a dwarf, but you can never vanquish one."),
+		S("It is easy to fool a goblin, but even easier to kill one."),
+		S("The best place to hide something precious is in your beard."),
+		S("Evil breeds in the guts of the lazy."),
+		S("The stones will sing if you let them."),
+		S("Drinking contests with humans are unbearable. They drink, they collapse, \nand we have to drag them back to their homes. The next day they never remember losing."),
+		S("A spear is not a dwarven weapon, but it will kill all the same."),
 	}
 }
 
@@ -108,26 +117,26 @@ lottmobs.elf = {
 		"Mistriel", "Ganis", "Mithes", "Loboril"
 	},
 	messages = {
-		"Welcome to our lovely forest home, weary traveler. Refresh yourself here.",
-		"Sauron grows in power. Shall we be able to vanquish him again?",
-		"We are a peace loving people, but if we are angered, our wrath is terrible!",
-		"Rest among us and prepare yourself, for war is imminent.",
-		"If you wish to buy goods from us, there are certain traders who wander our land.",
-		"Beware! Our society, and all societies, are on the edge of a knife blade - one false move and all will end, and Sauron will rule supreme.",
-		"Êl síla erin lû e-govaned vîn.",
-		"It is perilous to study too deeply the arts of the Enemy, for good or for ill. But such falls and betrayals, alas, have happened before.",
-		"Do not meddle in the affairs of Wizards, for they are subtle and quick to anger.",
-		"The praise of the praiseworthy is above all rewards.",
-		"The old that is strong does not wither, deep roots are not reached by the frost.",
-		"I will not say 'do not weep,' for not all tears are an evil.",
-		"For even the very wise cannot see all ends.",
-		"Faithless is he who says farewell when the road darkens.",
+		S("Welcome to our lovely forest home, weary traveler. Refresh yourself here."),
+		S("Sauron grows in power. Shall we be able to vanquish him again?"),
+		S("We are a peace loving people, but if we are angered, our wrath is terrible!"),
+		S("Rest among us and prepare yourself, for war is imminent."),
+		S("If you wish to buy goods from us, there are certain traders who wander our land."),
+		S("Beware! Our society, and all societies, are on the edge of a knife blade - one false move and all will end, and Sauron will rule supreme."),
+		S("Êl síla erin lû e-govaned vîn."),
+		S("It is perilous to study too deeply the arts of the Enemy, for good or for ill. But such falls and betrayals, alas, have happened before."),
+		S("Do not meddle in the affairs of Wizards, for they are subtle and quick to anger."),
+		S("The praise of the praiseworthy is above all rewards."),
+		S("The old that is strong does not wither, deep roots are not reached by the frost."),
+		S("I will not say 'do not weep,' for not all tears are an evil."),
+		S("For even the very wise cannot see all ends."),
+		S("Faithless is he who says farewell when the road darkens."),
 		"Aa' lasser en lle coia orn n' omenta gurtha.",
-		"Sweet water and light laughter till next we meet.",
-		"Aa' menealle nauva ar' malta.",
-		"May the wind fill your sails.",
-		"Do not scorn pity that is the gift of a gentle heart, Éowyn!",
-		"Someone else always has to carry on the story.",
+		S("Sweet water and light laughter till next we meet."),
+		S("Aa' menealle nauva ar' malta."),
+		S("May the wind fill your sails."),
+		S("Do not scorn pity that is the gift of a gentle heart, Éowyn!"),
+		S("Someone else always has to carry on the story."),
 	}
 }
 
@@ -171,13 +180,13 @@ lottmobs.hobbit = {
 		"Hobson", "Ilberic", "Largo", "Madoc", "Orgulas", "Rorimac"
 	},
 	messages = {
-		"Ah, what a lovely land we have, so peaceful, so beautiful.",
-		"There's nothing quite like the smell of pipe smoke rising on a cold October morning, is there?",
-		"If you are in need of any food, there are traders who wander around and they usually have a good stock.",
-		"If you are thinking that you'll find adventures here, think again! Good day!",
-		"We hear tales of war, but they cannot be more than tales - like that of the Oliphaunt.",
-		"Go not to the Elves for counsel, for they will say both no and yes.",
-		"Food is meant to be enjoyed, not rushed. Don't just eat a little here and a little there, sit down for a proper meal sometimes...",
+		S("Ah, what a lovely land we have, so peaceful, so beautiful."),
+		S("There's nothing quite like the smell of pipe smoke rising on a cold October morning, is there?"),
+		S("If you are in need of any food, there are traders who wander around and they usually have a good stock."),
+		S("If you are thinking that you'll find adventures here, think again! Good day!"),
+		S("We hear tales of war, but they cannot be more than tales - like that of the Oliphaunt."),
+		S("Go not to the Elves for counsel, for they will say both no and yes."),
+		S("Food is meant to be enjoyed, not rushed. Don't just eat a little here and a little there, sit down for a proper meal sometimes..."),
 	}
 }
 
@@ -221,12 +230,12 @@ lottmobs.human = {
 		"Gilraen", "Írimon", "Minardil", "Oromendil", "Tarcil", "Vorondil"
 	},
 	messages = {
-		"War comes swiftly... We are preparing, but are we doing enough?",
-		"The noble race of man rises in the world! Even the dwarves are starting to show interest in some of our goods.",
-		"Are you willing to fight with us? We have much to lose, but much to gain also! We must rally together.",
-		"Don't listen to those who say that all this talk of war will come to nothing, for we are at war now.",
-		"We suffer raids from orcs, and other evil things, yet we do nothing! We must act, and act with force!",
-		"Life here is far from normal. We wish for peace, yet the only way we can get peace is through war...",
+		S("War comes swiftly... We are preparing, but are we doing enough?"),
+		S("The noble race of man rises in the world! Even the dwarves are starting to show interest in some of our goods."),
+		S("Are you willing to fight with us? We have much to lose, but much to gain also! We must rally together."),
+		S("Don't listen to those who say that all this talk of war will come to nothing, for we are at war now."),
+		S("We suffer raids from orcs, and other evil things, yet we do nothing! We must act, and act with force!"),
+		S("Life here is far from normal. We wish for peace, yet the only way we can get peace is through war..."),
 	}
 }
 lottmobs.orc = {
@@ -236,7 +245,7 @@ lottmobs.orc = {
                 "Othrod", "Radbug", "Shagrat", "Ufthak", "Uglúk"
         },
         messages = {
-                "DIE!!!", "Urrrrrrrrrrrrrghhhhhhhhhhhhhhhhhhh!!",
-                "Arrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr!", "KILL! KILL! KILL!"
+                S("DIE!!!"), S("Urrrrrrrrrrrrrghhhhhhhhhhhhhhhhhhh!!"),
+                S("Arrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr!"), S("KILL! KILL! KILL!")
         },
 }
