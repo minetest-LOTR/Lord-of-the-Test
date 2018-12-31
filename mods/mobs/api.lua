df57d9e0156ef61bcdd5475a1272c497a84de4c2
Isidor Zeuner <minetest@quidecco.de>
2018-12-31 16:47:49 +0100

    upgrade intllib interface

--- mods/mobs/api.lua
+++ mods/mobs/api.lua
@@ -21,25 +21,14 @@ mobs = {}
 mobs.mod = "redo"
 
 -- Intllib
-local S
-if minetest.get_modpath("intllib") then
-	S = intllib.Getter()
-else
-	S = function(s, a, ...)
-		if a == nil then
-			return s
-		end
-		a = {a, ...}
-		return s:gsub("(@?)@(%(?)(%d+)(%)?)",
-			function(e, o, n, c)
-				if e == "" then
-					return a[tonumber(n)] .. (o == "" and c or "")
-				else
-					return "@" .. o .. n .. c
-				end
-			end)
-	end
-end
+local MP = minetest.get_modpath(
+    minetest.get_current_modname(
+    )
+)
+
+local S, NS = dofile(
+    MP .. "/intllib.lua"
+)
 mobs.intllib = S
 
 -- Invisibility mod
